68ba610f269e4b1ae3b99aa6801fcc4f0cc74dac
Jordan <jordanfurneri57@gmail.com>
2023-02-22 17:43:17 +0100

    Modification du code avec des l'ajout de commentaire

--- FrontEnd/JS/script.js
+++ FrontEnd/JS/script.js
@@ -1,35 +1,49 @@
-const actualGallery = document.querySelector(".gallery");
+const response = await fetch("http://localhost:5678/api/works");
+const pieces = await response.json();
 
-fetch("http://localhost:5678/api/works")
-  .then((response) => response.json())
-  .then((data) => {
-    data.forEach((project) => {
-      // createElement figure
-      const projectGallery = document.createElement("figure");
+console.log(pieces);
 
-      // Ajoutez l'image du projet
-      const imageGallery = document.createElement("img");
-      imageGallery.src = project.imageUrl;
-      projectGallery.appendChild(imageGallery);
+for (let i = 0; i < pieces.length; i++) {
+  // Index du parcours de la boucles
+  const article = pieces[i];
+  // Récup de l'élément du DOM qui accueillera les figures
+  const actualGallery = document.querySelector(".gallery");
 
-      // Ajoutez le titre du projet
-      const titleGallery = document.createElement("figcaption");
-      titleGallery.textContent = project.title;
-      projectGallery.appendChild(titleGallery);
+  const figureGallery = document.createElement("figure");
+  // createElement figure
+  const imageGallery = document.createElement("img");
+  imageGallery.src = article.imageUrl;
+  // createElement figcaption
+  const titleGallery = document.createElement("figcaption");
+  titleGallery.textContent = article.title;
+  // Récupe de categoryID
+  const idGallery = article.categoryId;
+  //  Rattacher les éléments a la div ".gallery"
+  actualGallery.appendChild(figureGallery);
+  // Rattacher les éléments images et title a la balise figure et figcaption
+  figureGallery.appendChild(imageGallery);
+  figureGallery.appendChild(titleGallery);
+}
 
-      // Ajoutez l'élément de projet au conteneur de projets
-      actualGallery.appendChild(projectGallery);
-    });
-  });
+//Ajout des eventListener pour les filtres
 
-// const actualFilter = document.querySelector("#portfolio");
+// Tous
+const btnAll = document.querySelector(".btn-all");
+btnAll.addEventListener("click", function () {
+  console.log("Tous");
+});
+// Objets
+const btnObjets = document.querySelector(".btn-objets");
+btnObjets.addEventListener("click", function () {});
 
-// fetch("http://localhost:5678/api/categories")
-//   .then((responsefilter) => responsefilter.json())
-//   .then((filter) => {
-//     console.log(filter);
- 
-//   });
+// Appartements
+const btnApps = document.querySelector(".btn-apps");
+btnApps.addEventListener("click", function () {
+  console.log("Appartements");
+});
 
-
-  
+// Hôtels & restaurants
+const btnHotel = document.querySelector(".btn-hotel");
+btnHotel.addEventListener("click", function () {
+  console.log("Hotel");
+});
